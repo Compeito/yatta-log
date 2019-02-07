dd373f4a7b539ad2ef04642039da5a3ee9ef8056
compeito <com0806peito@icloud.com>
2019-02-07 14:59:59 +0900

    BaseForm作成

---
+++ src/components/BaseForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-form v-model="isValid">
+    <v-container>
+      <v-layout column>
+        <component
+          v-for="field in fields"
+          v-model="field.model"
+          item-text="text"
+          item-value="value"
+          :items="field.items"
+          :key="field.label"
+          :is="field.component"
+          :rules="field.rules"
+          :label="field.label"
+          :counter="field.counter"
+          :required="field.isRequired"
+        ></component>
+        <slot></slot>
+      </v-layout>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  props: {
+    'fields': Array
+  },
+  data() {
+    return {
+      isValid: false,
+    }
+  }
+}
+</script>
