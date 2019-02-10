833af86948a6572b57886c4ff1c9f14099bfb4c0
Compeito <com0806peito@icloud.com>
2019-02-10 21:19:45 +0900

    ユーザー情報をDBに自動保存

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -19,12 +19,17 @@ import Navbar from '~/components/Navbar'
 import Toolbar from '~/components/Toolbar'
 import AlertSnackbar from '~/components/AlertSnackbar'
 import auth from '~/plugins/auth'
+import firebase from '~/plugins/firebase'
 
 export default {
   components: { Navbar, Toolbar, AlertSnackbar },
   mounted() {
     auth().then(user => {
       this.$store.commit('user/set', user.uid)
+      firebase.firestore().collection('users').doc(user.uid).set({
+        displayName: user.displayName,
+        photoURL: user.photoURL
+      })
     })
   }
 }
