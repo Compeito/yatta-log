f8a1d663a1e41eb3d0f8d7dd9b34a15cb8450026
Compeito <com0806peito@icloud.com>
2019-02-14 23:54:26 +0900

    更新

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,20 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: pkg.title,
     htmlAttrs: {
-      lang: 'ja'
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'og:title', property: 'og:title', content: pkg.title },
+      { hid: 'og:image', property: 'og:image', content: 'https://yatta-log.firebaseapp.com/icon.png' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:title', name: 'twitter:title', content: pkg.title },
+      { hid: 'twitter:image', name: 'twitter:image', content: 'https://yatta-log.firebaseapp.com/icon.png' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -80,15 +86,15 @@ module.exports = {
     //   dev: true
     // },
     manifest: {
-      name: 'PWATest',
-      short_name: 'PWA',
-      title: 'PWATest',
-      'og:title': 'PWATest',
-      description: 'PWATest',
-      'og:description': 'PWATest',
+      name: pkg.name,
+      short_name: pkg.title,
+      title: pkg.title,
+      'og:title': pkg.title,
+      description: pkg.description,
+      'og:description': pkg.description,
       lang: 'ja',
-      theme_color: '#ffffff',
-      background_color: '#ffffff'
+      theme_color: '#eeeeee',
+      background_color: '#eeeeee'
     },
 
     /*
